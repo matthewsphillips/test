0f2f172ed5ab9b1511f6f5dbf08b50b50caf2180
Matthew Phillips <matthewsphillips@me.com>
2023-11-15 12:20:24 -0800

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Matthew Phillips on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
